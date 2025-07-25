d7a7bbdf5aa5636717f366cbb9c9aead7cf83384
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-07-25 14:34:40 +0200

    create theme with colors and font

---
+++ src/theme/Theme.jsx
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+const theme = createTheme({
+palette: {
+primary: {
+main: '#663bb4',
+black: '#000000',
+white: '#ffffff',
+fontfamily: 'Roboto, sans-serif',
+},
+
+},
+});
+export default theme;
